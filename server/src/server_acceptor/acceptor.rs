fbc305749549707dea21ffc655ddd16d6ba3f22f
Juan Martin de la Cruz <jdelacruz@fi.uba.ar>
2025-05-20 16:36:16 -0300

    refactor: mejorar la estructura del código en Acceptor y Server
    TODO: Corregir el socket_reader

--- server/src/server_acceptor/acceptor.rs
+++ server/src/server_acceptor/acceptor.rs
@@ -1,8 +1,10 @@
 use actix::prelude::*;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use common::logger::{Logger};
+use common::logger::Logger;
 use crate::server_actors::server_actor::Server;
+use common::messages::shared_messages::StartRunning;
+
 
 pub struct Acceptor {
     pub addr: SocketAddr,
@@ -11,35 +13,31 @@ pub struct Acceptor {
 
 impl Acceptor {
     pub fn new(addr: SocketAddr) -> Self {
-        Self { addr, logger: Arc::new(Logger::new("ACCEPTOR")) }
+        Self {
+            addr,
+            logger: Arc::new(Logger::new("ACCEPTOR")),
+        }
     }
 
     pub async fn start(&self) -> std::io::Result<()> {
-        
         let listener = tokio::net::TcpListener::bind(self.addr).await?;
-        self.logger.info(format!(
-            "Acceptor started, listening on {}",
-            self.addr
-        ));
+        self.logger.info(format!("Acceptor started, listening on {}", self.addr));
+        self.accept_connections(listener).await
+    }
 
+    async fn accept_connections(
+        &self,
+        listener: tokio::net::TcpListener,
+    ) -> std::io::Result<()> {
         loop {
             match listener.accept().await {
                 Ok((stream, client_addr)) => {
-                    self.logger.info(format!(
-                        "Acceptor received connection from {:?}",
-                        client_addr
-                    ));
-                    Server::new(
-                        stream,
-                        self.addr,
-                        client_addr,   
-                    );
+                    self.logger.info(format!("Accepted connection from {}", client_addr));
+                    let server_addr = Server::new_empty(stream, self.addr, client_addr);
+                    server_addr.do_send(StartRunning);
                 }
                 Err(e) => {
-                    self.logger.error(format!(
-                        "Acceptor failed accepting connection: {}",
-                        e
-                    ));
+                    self.logger.warn(format!("Failed to accept connection: {}", e));
                 }
             }
         }
--- server/src/server_actors/server_actor.rs
+++ server/src/server_actors/server_actor.rs
@@ -1,59 +1,80 @@
 use actix::prelude::*;
+use common::messages::shared_messages::StartRunning;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use common::logger::{Logger};
+use common::logger::Logger;
 use common::network::socket_reader::{IncomingLine, SocketReader};
 use common::network::socket_writer::{SocketWriter, TCPMessage};
 use serde::Serialize;
+use tokio::net::TcpStream;
 
 pub struct Server {
     pub addr: SocketAddr,
     pub client_addr: SocketAddr,
-    pub socket_writer: Arc<Addr<SocketWriter>>,
     pub logger: Arc<Logger>,
+    pub socket_writer: Option<Arc<Addr<SocketWriter>>>,
+    pub stream: Option<TcpStream>,
+}
+
+impl Server {
+    pub fn new_empty(
+        stream: TcpStream,
+        srv_addr: SocketAddr,
+        client_addr: SocketAddr,
+    ) -> Addr<Self> {
+        Server::create(move |_ctx| {
+            Server {
+                addr: srv_addr,
+                client_addr,
+                logger: Arc::new(Logger::new(format!("SERVER {}", client_addr))),
+                socket_writer: None,
+                stream: Some(stream),
+            }
+        })
+    }
+
+    fn send_message<T: Serialize>(&self, msg: &T) {
+        if let Some(writer) = &self.socket_writer {
+            if let Ok(json) = serde_json::to_string(msg) {
+                writer.do_send(TCPMessage(json));
+            } else {
+                self.logger.warn("Failed to serialize message");
+            }
+        } else {
+            self.logger.warn("Attempted to send message without active writer");
+        }
+    }
 }
 
 impl Actor for Server {
     type Context = Context<Self>;
 }
 
-impl Server {
-    pub fn new(
-        stream: tokio::net::TcpStream,
-        srv_addr: SocketAddr,
-        client_addr: SocketAddr,
-    ) -> Addr<Self> {
-        Server::create(move |ctx| {
-            let (read_half, write_half) = stream.into_split();
-            let socket_writer = Arc::new(SocketWriter::new(write_half).start());
-            let _socket_reader = SocketReader::new(read_half, ctx.address()).start();
+impl Handler<StartRunning> for Server {
+    type Result = ();
 
-            Server {
-                addr: srv_addr,
-                client_addr,
-                socket_writer,
-                logger: Arc::new(Logger::new(format!("SERVER {}", client_addr))),
-            }
-        })
-    }
-    
-    fn send_message<T: Serialize>(&self, msg: &T) {
-        if let Ok(json) = serde_json::to_string(msg) {
-            self.socket_writer.do_send(TCPMessage(json));
+    fn handle(&mut self, _msg: StartRunning, ctx: &mut Self::Context) {
+        if let Some(stream) = self.stream.take() {
+            let (read_half, write_half) = stream.into_split();
+
+            let writer = Arc::new(SocketWriter::new(write_half).start());
+            self.socket_writer = Some(writer.clone());
+
+            let _reader = SocketReader::new(read_half, ctx.address()).start();
+
+            self.logger.info("Server is now running".to_string());
         } else {
-            self.logger.warn("Failed to serialize message");
+            self.logger.warn("StartRunning received, but no stream available");
         }
     }
-    
 }
 
 impl Handler<IncomingLine> for Server {
     type Result = ();
-    fn handle(&mut self, msg: IncomingLine, ctx: &mut Self::Context) -> Self::Result {
-        print!("{}: Received message: {}\n", self.client_addr, msg.0);
-        
+
+    fn handle(&mut self, msg: IncomingLine, _ctx: &mut Self::Context) -> Self::Result {
+        println!("{}: Received message: {}", self.client_addr, msg.0);
     }
 }
-
 
 
